ee46e21779558eca962d7c9fb439bc12f38ba41b
刘祥 <lx_myall@163.com>
2021-07-26 01:02:12 +0800

    update READEME

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 学习系列
+## Talk is cheap. Show me the code.
--- hooks/hooks01/src/index.js
+++ hooks/hooks01/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import State from "./State";
-import State from "./State";
+import State2 from "./State2";
 import Effect from "./Effect";
 import Context from "./Context";
 ReactDOM.render(
